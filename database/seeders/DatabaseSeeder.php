a48f13b9537dd1098e898ce2beb279ae7c00a49f
ruslanzhuk <ruslanzhukotynskyi@gmail.com>
2024-08-19 14:51:25 +0200

    Baza danych

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+        $this->call(ohohoTableFootballMatchesTableSeeder::class);
         $this->call(TableFootballMatchesTableSeeder::class);
     }
 }
